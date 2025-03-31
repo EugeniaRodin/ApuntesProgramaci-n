31945c2ced895220bcdb1df1d31fe890cd977dcc
Eugenia Rodín <145973464+EugeniaRodin@users.noreply.github.com>
2025-03-31 11:01:48 +0200

    actualizacion31/3

---
+++ ampliacionProg/src/teoría2/Calculator.java
@@ -0,0 +1,22 @@
+package teoría2;
+
+public class Calculator {
+	interface IntegerMath {
+		int operation(int a, int b);
+	}
+
+	public int operateBinary(int a, int b, IntegerMath op) {
+		return op.operation(a, b);
+	}
+
+	public static void main(String... args) {
+
+		Calculator myApp = new Calculator();
+		IntegerMath addition = (a, b) -> a + b;
+		IntegerMath subtraction = (a, b) -> a - b;
+		IntegerMath product = (a, b) -> a * b;
+		System.out.println("40 + 2 = " + myApp.operateBinary(40, 2, addition));
+		System.out.println("20 - 10 = " + myApp.operateBinary(20, 10, subtraction));
+		System.out.println("20 * 10 = " + myApp.operateBinary(20, 10, product));
+	}
+}
